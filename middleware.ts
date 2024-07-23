c3374d8258774a2e58f23db8fdf60dd1670b4615
yhwabillie <yhwabillie93@gmail.com>
2024-07-24 01:07:45 +0900

    feat: middleware reset

--- middleware.ts
+++ middleware.ts
@@ -2,21 +2,21 @@ import { NextRequest, NextResponse } from 'next/server'
 import { cookies } from 'next/headers'
 export { default } from 'next-auth/middleware'
 
-export async function middleware(request: NextRequest) {
-  const accessToken = cookies().get('next-auth.session-token')
+// export async function middleware(request: NextRequest) {
+//   const accessToken = cookies().get('next-auth.session-token')
 
-  if (!accessToken) {
-    if (request.nextUrl.pathname.startsWith('/profile')) {
-      return NextResponse.redirect(new URL('/signIn', request.url))
-    }
-  }
+//   if (!accessToken) {
+//     if (request.nextUrl.pathname.startsWith('/profile')) {
+//       return NextResponse.redirect(new URL('/signIn', request.url))
+//     }
+//   }
 
-  if (accessToken) {
-    if (request.nextUrl.pathname.startsWith('/signIn') || request.nextUrl.pathname.startsWith('/signUp')) {
-      return NextResponse.redirect(new URL('/', request.url))
-    }
-  }
-}
+//   if (accessToken) {
+//     if (request.nextUrl.pathname.startsWith('/signIn') || request.nextUrl.pathname.startsWith('/signUp')) {
+//       return NextResponse.redirect(new URL('/', request.url))
+//     }
+//   }
+// }
 
 export const config = {
   matcher: ['/signIn/:path*', '/signUp/:path*', '/profile/:path*'],
