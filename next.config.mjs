a95613fb05ed63d99a12f62009297bd65cdbfaa7
yhwabillie <yhwabillie93@gmail.com>
2024-07-24 08:19:15 +0900

    feat: prisma error next config fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,13 @@
-import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin'
+// import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()]
-    }
-
-    return config
-  },
+  // webpack: (config, { isServer }) => {
+  //   if (isServer) {
+  //     config.plugins = [...config.plugins, new PrismaPlugin()]
+  //   }
+  //   return config
+  // },
 }
 
 export default nextConfig
