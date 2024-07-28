1dbc69b53ce9ee718b96ac783594edf40f9004e2
yhwabillie <yhwabillie93@gmail.com>
2024-07-28 22:28:14 +0900

    feat: query parameter get

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -1,14 +1,23 @@
-'use server'
-
-import ResetPasswordPage from '@/lib/components/ResetPassword'
-import { Suspense } from 'react'
+'use client'
+import { useSearchParams } from 'next/navigation'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 export default async function Page() {
+  // useSearchParams 훅을 사용하여 쿼리 파라미터에서 토큰을 가져옵니다.
+  const searchParams = useSearchParams()
+  const router = useRouter()
+  const token = searchParams.get('token') // 쿼리 파라미터에서 토큰을 가져옵니다.
+
+  useEffect(() => {
+    if (!token) {
+      console.log('No token provided')
+    }
+  }, [token])
+
   return (
     <div>
-      <Suspense fallback={<div>Loading...</div>}>
-        <ResetPasswordPage />
-      </Suspense>
+      <h1>Reset PW</h1>
     </div>
   )
 }
