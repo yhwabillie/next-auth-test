848c5916194877dbc6e3245215308b91c793f842
yhwabillie <yhwabillie93@gmail.com>
2024-08-11 16:44:24 +0900

    feat: alert modal 컴포넌트 메시지 작성

--- lib/components/ModalProvider.tsx
+++ lib/components/ModalProvider.tsx
@@ -10,15 +10,19 @@ export const ModalProvider = () => {
   const { modals: address_modals } = useAddressDataStore()
   const { modals: order_modals } = useOrderDataStore()
 
+  const alert_messages = {
+    addressList: {
+      error: '해당 주소 데이터로 생성된 주문/배송 데이터가 있습니다. <br /> 배송완료 후 삭제해주세요.',
+    },
+  }
+
   return (
     <>
       {address_modals.addNewAddress && <AddNewAddressForm />}
       {address_modals.editAddress && <AddressUpdateForm />}
       {address_modals.postcode && <PostCodeModal />}
       {order_modals.changeAddress && <ChangeOrderAddress />}
-      {address_modals.alert && (
-        <AlertErrorModal message={'해당 주소 데이터로 생성된 주문/배송 데이터가 있습니다. <br /> 배송완료 후 삭제해주세요.'} />
-      )}
+      {address_modals.alert && <AlertErrorModal message={alert_messages.addressList.error} />}
     </>
   )
 }
