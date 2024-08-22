7140a03764b6750ae2b79493652056ef6f9c48bf
yhwabillie <yhwabillie93@gmail.com>
2024-08-22 14:32:06 +0900

    build: maxDuration auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth'
 import authOptions from '@/lib/auth'
-export const maxDuration = 300
+// export const maxDuration = 300
 export const dynamic = 'force-dynamic'
 
 const handler = NextAuth(authOptions)
