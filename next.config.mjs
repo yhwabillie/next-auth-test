25b991740e805590a05e79ecba7b24d1b7228c79
yhwabillie <yhwabillie93@gmail.com>
2024-08-31 12:04:38 +0900

    feat: cdn이미지 캐시 정책 설정

--- next.config.mjs
+++ next.config.mjs
@@ -17,9 +17,32 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'fluplmlpoyjvgxkldyfh.supabase.co',
         port: '',
-        pathname: '/storage/v1/object/public/**',
+        pathname: '/storage/v1/object/public/**', // 모든 public 경로의 이미지에 적용됩니다.
       },
     ],
+    formats: ['image/avif', 'image/webp'], // 웹 최적화 이미지 포맷 지정
+  },
+  async headers() {
+    return [
+      {
+        source: '/_next/image(.*)', // Next.js 이미지 최적화 라우트에 적용
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable', // 1년 동안 캐시, 변경되지 않음
+          },
+        ],
+      },
+      {
+        source: '/storage/v1/object/public/(.*)', // Supabase 이미지 경로에 적용
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=86400', // 1일 동안 캐시
+          },
+        ],
+      },
+    ]
   },
 }
 
