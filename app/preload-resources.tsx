b9ff19a766d42d4dc1cdc6cd51d1af850ee79223
yhwabillie <yhwabillie93@gmail.com>
2024-08-31 17:03:45 +0900

    feat: LCP 이미지 프리로드

--- app/preload-resources.tsx
+++ app/preload-resources.tsx
@@ -5,6 +5,12 @@ import ReactDOM from 'react-dom'
 export function PreloadResources() {
   ReactDOM.preload('/fonts/Pretendard-Regular.subset.woff2', {
     as: 'font',
+    type: 'font/woff2',
+    fetchPriority: 'high',
+  })
+
+  ReactDOM.preload('https://fluplmlpoyjvgxkldyfh.supabase.co/storage/v1/object/public/next-auth-test/banners/banner-1.webp', {
+    as: 'image',
     fetchPriority: 'high',
   })
 
