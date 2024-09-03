68ee45df81e208fbdd322bf9d728fed32e636fa8
yhwabillie <yhwabillie93@gmail.com>
2024-09-03 21:39:52 +0900

    feat: font preload 주석처리

--- app/preload-resources.tsx
+++ app/preload-resources.tsx
@@ -2,11 +2,11 @@
 import ReactDOM from 'react-dom'
 
 export function PreloadResources() {
-  ReactDOM.preload('/fonts/Pretendard-Regular.subset.woff2', {
-    as: 'font',
-    type: 'font/woff2',
-    fetchPriority: 'high',
-  })
+  // ReactDOM.preload('/fonts/Pretendard-Regular.subset.woff2', {
+  //   as: 'font',
+  //   type: 'font/woff2',
+  //   fetchPriority: 'high',
+  // })
 
   return null
 }
