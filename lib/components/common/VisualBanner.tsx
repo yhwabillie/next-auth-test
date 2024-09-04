6b83e6370fb0bcc4b3427d3f2bc512097916cc6f
yhwabillie <yhwabillie93@gmail.com>
2024-09-04 13:07:54 +0900

    feat: next 이미지 컴포넌트만 사용

--- lib/components/common/VisualBanner.tsx
+++ lib/components/common/VisualBanner.tsx
@@ -221,9 +221,9 @@ export const VisualBanner = () => {
               </motion.div>
 
               <picture>
-                <source media="(max-width: 767px)" srcSet={item.mobile_image} />
+                {/* <source media="(max-width: 767px)" srcSet={item.mobile_image} />
                 <source media="(max-width: 1279px)" srcSet={item.tablet_image} />
-                <source media="(min-width: 1280px)" srcSet={item.desktop_image} />
+                <source media="(min-width: 1280px)" srcSet={item.desktop_image} /> */}
 
                 <motion.div variants={imageVariants} initial="hidden" animate={isActive ? 'visible' : 'hidden'} className="absolute inset-0">
                   <Image
