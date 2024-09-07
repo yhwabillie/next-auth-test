ba7799d230a86689fdbfe78162767b9413342dde
yhwabillie <yhwabillie93@gmail.com>
2024-09-07 22:56:03 +0900

    feat: tailwind config 중복값 삭제

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,7 @@ module.exports = {
   important: true,
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
     './lib/components/**/*.{js,ts,jsx,tsx,mdx}',
-
-    // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
