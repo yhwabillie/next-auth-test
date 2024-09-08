817d402c57ec59fe609f342f1527110ef93e3bd2
yhwabillie <yhwabillie93@gmail.com>
2024-09-08 10:55:01 +0900

    feat: 슬라이더 배너 이미지 sizes 미디어쿼리 변경

--- lib/components/common/VisualBanner.tsx
+++ lib/components/common/VisualBanner.tsx
@@ -212,7 +212,7 @@ export const VisualBanner = () => {
                     src={item.desktop_image}
                     alt={item.banner_alt}
                     fill
-                    sizes="(min-width: 320px) 100vw, (max-width: 767px) 501px, (max-width: 1279px) 843px, (min-width: 1280px) 960px"
+                    sizes="(min-width: 1520px) 960px, (min-width: 380px) calc(63.13vw + 13px), calc(21.67vw + 162px)"
                     className="object-cover"
                     priority={true}
                     loading={'eager'}
